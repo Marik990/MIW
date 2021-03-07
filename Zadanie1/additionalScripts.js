a4575918c07ec678128b5b0c0032f32f9d8f3d97
MJ <150760@student.uwm.edu.pl>
2021-03-07 21:44:17 +0100

    Created Data validation

--- Zadanie1/additionalScripts.js
+++ Zadanie1/additionalScripts.js
@@ -1,8 +1,4 @@
-$("#upload-data-btn").click(function() {
-    $('html,body').animate({
-        scrollTop: $("#board").offset().top
-    }, 'slow');
-});
+document.getElementById('upload-data-form').reset()
 
 $('#back-btn').on("click",function(){
     $('html,body').animate({
@@ -12,6 +8,10 @@ $('#back-btn').on("click",function(){
 
 $('#upload-data-btn').on('click',function () {
     readDataFile(document.getElementById('data-file'))
+
+    $('html,body').animate({
+        scrollTop: $("#board").offset().top
+    }, 'slow');
 })
 
 function displayText(text, elementId) {
@@ -23,67 +23,77 @@ function displayHTML(html, elementId) {
 }
 
 function readDataFile(filePath) {
-    if(checkFileAPI() === false)
-        return false
-    var reader = new FileReader()
-    var output = ""
-    if(filePath.files && filePath.files[0]) {
-        reader.onload = function (e) {
-            output = e.target.result;
-            processDataFileContent(output);
-        };//end onload()
-        reader.readAsText(filePath.files[0]);
-    }//end if html5 filelist support
-    else if(ActiveXObject && filePath) { //fallback to IE 6-8 support via ActiveX
-        try {
-            reader = new ActiveXObject("Scripting.FileSystemObject");
-            var file = reader.OpenTextFile(filePath, 1); //ActiveX File Object
-            output = file.ReadAll(); //text contents of file
-            file.Close(); //close file "input stream"
-            processDataFileContent(output);
-        } catch (e) {
-            if (e.number == -2146827859) {
-                alert('Unable to access local files due to browser security settings. ' +
-                    'To overcome this, go to Tools->Internet Options->Security->Custom Level. ' +
-                    'Find the setting for "Initialize and script ActiveX controls not marked as safe" and change it to "Enable" or "Prompt"');
+    try {
+        if (checkFileAPI() === false)
+            return false
+        let reader = new FileReader()
+        let output = ""
+        if (filePath.files && filePath.files[0]) {
+            reader.onload = function (e) {
+                output = e.target.result;
+                processDataFileContent(output);
+            };//end onload()
+            reader.readAsText(filePath.files[0]);
+        }//end if html5 filelist support
+        else if (ActiveXObject && filePath) { //fallback to IE 6-8 support via ActiveX
+            try {
+                reader = new ActiveXObject("Scripting.FileSystemObject");
+                let file = reader.OpenTextFile(filePath, 1); //ActiveX File Object
+                output = file.ReadAll(); //text contents of file
+                file.Close(); //close file "input stream"
+                processDataFileContent(output);
+            } catch (e) {
+                if (e.number == -2146827859) {
+                    alert('Unable to access local files due to browser security settings. ' +
+                        'To overcome this, go to Tools->Internet Options->Security->Custom Level. ' +
+                        'Find the setting for "Initialize and script ActiveX controls not marked as safe" and change it to "Enable" or "Prompt"');
+                }
             }
+        } else { //this is where you could fallback to Java Applet, Flash or similar
+            return false;
         }
     }
-    else { //this is where you could fallback to Java Applet, Flash or similar
-        return false;
+    catch (e) {
+        alert(`Błąd: Nie można załadować pliku z danymi!`)
+        throw new Error(`${e.message}`)
     }
     return true;
 }
 
 function readConfigFile(filePath) {
-    if(checkFileAPI() === false)
-        return false
-    var reader = new FileReader()
-    var output = ""
-    if(filePath.files && filePath.files[0]) {
-        reader.onload = function (e) {
-            output = e.target.result;
-            processConfigFile(output);
-        };//end onload()
-        reader.readAsText(filePath.files[0]);
-    }//end if html5 filelist support
-    else if(ActiveXObject && filePath) { //fallback to IE 6-8 support via ActiveX
-        try {
-            reader = new ActiveXObject("Scripting.FileSystemObject");
-            var file = reader.OpenTextFile(filePath, 1); //ActiveX File Object
-            output = file.ReadAll(); //text contents of file
-            file.Close(); //close file "input stream"
-            processConfigFile(output);
-        } catch (e) {
-            if (e.number == -2146827859) {
-                alert('Unable to access local files due to browser security settings. ' +
-                    'To overcome this, go to Tools->Internet Options->Security->Custom Level. ' +
-                    'Find the setting for "Initialize and script ActiveX controls not marked as safe" and change it to "Enable" or "Prompt"');
+    try {
+        if (checkFileAPI() === false)
+            return false
+        let reader = new FileReader()
+        let output = ""
+        if (filePath.files && filePath.files[0]) {
+            reader.onload = function (e) {
+                output = e.target.result;
+                processConfigFile(output);
+            };//end onload()
+            reader.readAsText(filePath.files[0]);
+        }//end if html5 filelist support
+        else if (ActiveXObject && filePath) { //fallback to IE 6-8 support via ActiveX
+            try {
+                reader = new ActiveXObject("Scripting.FileSystemObject");
+                let file = reader.OpenTextFile(filePath, 1); //ActiveX File Object
+                output = file.ReadAll(); //text contents of file
+                file.Close(); //close file "input stream"
+                processConfigFile(output);
+            } catch (e) {
+                if (e.number == -2146827859) {
+                    alert('Unable to access local files due to browser security settings. ' +
+                        'To overcome this, go to Tools->Internet Options->Security->Custom Level. ' +
+                        'Find the setting for "Initialize and script ActiveX controls not marked as safe" and change it to "Enable" or "Prompt"');
+                }
             }
+        } else { //this is where you could fallback to Java Applet, Flash or similar
+            return false;
         }
     }
-    else { //this is where you could fallback to Java Applet, Flash or similar
-        return false;
+    catch (e) {
+        alert(`Błąd: Nie można załadować pliku konfiguracyjnego!`)
+        throw new Error(`${e.message}`)
     }
     return true;
 }
