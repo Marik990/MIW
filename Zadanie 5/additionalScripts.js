751b1b44638b6bfac920882ff9aadbfae9e2fb4d
MJ <150760@student.uwm.edu.pl>
2021-06-04 19:59:27 +0200

    Zadanie 5 - Light refactoring

--- Zadanie 5/additionalScripts.js
+++ Zadanie 5/additionalScripts.js
@@ -3,3 +3,24 @@ $('#back-btn').on("click",function(){
         scrollTop: 0
     }, 'slow');
 });
+
+$('#copy-btn').on("click",function(){
+    try{
+        //navigator.clipboard.writeText(document.getElementById("board").innerText)
+        const el = document.createElement('textarea');
+        el.value = document.getElementById("board").innerText
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+        alert("Skrypt został skopiowany pomyślnie!")
+    }
+    catch (e) {
+        alert(`Wystąpił błąd: ${e.message}`)
+        throw new Error(`${e.message}`)
+    }
+});
+
+function isAttributeInt(attribute) {
+    return (!isNaN(attribute) && !isNaN(parseInt(attribute)) && Number.isInteger(parseFloat(attribute)))
+}
